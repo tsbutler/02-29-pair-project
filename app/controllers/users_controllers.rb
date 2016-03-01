bac82d989311633b1e055cc4f0d5762169b599a6
Thorin Butler <thorin.butler@gmail.com>
2016-03-01 09:44:20 -0600

    Added CRUD framework to users_controllers

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,1 +1,19 @@
-#placeholder
+MyApp.get "/users/new" do
+  erb :"users/new"
+end
+
+MyApp.post "/users/create" do
+  redirect "/"
+end
+
+MyApp.get "/users" do
+  erb :"users/index"
+end
+
+MyApp.get "/users/:id" do
+  erb :"users/show"
+end
+
+MyApp.get "/users/:id/edit" do
+  erb :"users/edit"
+end
