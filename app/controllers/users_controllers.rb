0247e032e52eaf08c2e6d4e0d251616c179c1114
Thorin Butler <thorin.butler@gmail.com>
2016-03-01 14:00:02 -0600

    redirect issue

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -20,6 +20,7 @@ MyApp.post "/users/create" do
   else
     @user.save
   end
+  
 
   choices = [params["destination_id_1"], params["destination_id_2"], params["destination_id_3"], params["destination_id_4"], params["destination_id_5"]]
   choices.each do |choice|
