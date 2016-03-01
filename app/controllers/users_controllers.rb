3d3dcec3ecf6329101729393916e780eed8587a4
Thorin Butler <thorin.butler@gmail.com>
2016-03-01 11:28:05 -0600

    Able to edit user profile now

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -39,12 +39,22 @@ end
 
 MyApp.post "/users/:id/update" do
   @user = User.find_by_id(params[:id])
+  binding.pry
   @user.name = params[:name]
   @user.username = params[:username]
   @user.password = params[:password]
   @user.email = params[:email]
   @user.budget = params[:budget]
   @user.save
+
+  choices = [params["destination_id_1"], params["destination_id_2"], params["destination_id_3"], params["destination_id_4"], params["destination_id_5"]]
+  choices.each do |choice|
+    @choice = Choice.new
+    @choice.user_id = @user.id
+    @choice.destination_id = choice
+    @choice.save
+  end
+
   redirect "/users/#{@user.id}/profile"
 end
 
