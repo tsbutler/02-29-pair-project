cdbf89183a1a5e28f1c428c6d0d5391f923e4736
Thorin Butler <thorin.butler@gmail.com>
2016-03-01 09:53:00 -0600

    Added form to create new user

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -3,17 +3,37 @@ MyApp.get "/users/new" do
 end
 
 MyApp.post "/users/create" do
+  @user = User.new
+  @user.name = params[:name]
+  @user.username = params[:username]
+  @user.password = params[:password]
+  @user.email = params[:email]
+  @user.budget = params[:budget]
+  @user.save
   redirect "/"
 end
 
 MyApp.get "/users" do
+  @users = User.all
   erb :"users/index"
 end
 
 MyApp.get "/users/:id" do
+  @user = User.find_by_id(params[:id])
   erb :"users/show"
 end
 
 MyApp.get "/users/:id/edit" do
+  @user = User.find_by_id(params[:id])
   erb :"users/edit"
+end
+
+MyApp.post "/users/:id/update" do
+  @user = User.find_by_id(params[:id])
+  redirect "/"
+end
+
+MyApp.post "users/:id/delete" do
+  @user = User.find_by_id(params[:id])
+  redirect "/"
 end
