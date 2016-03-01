aacc91eccbbd5f39211383de2b232244d73d230f
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-01 16:57:26 -0600

    Login form is a thing

--- app/controllers/logins_controllers.rb
+++ app/controllers/logins_controllers.rb
@@ -1,1 +1,8 @@
-#placeholder
+MyApp.get "/logins/new" do
+  erb :"/logins/new"
+end
+
+MyApp.post "/logins/create" do
+  @user = User.find_by_username(params[:username])
+  redirect "/users/<%=@user.id%>/profile"
+end
