950a65b314857149fb7a247d778da37bb6e1a1cf
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-01 21:21:49 -0600

    Login C+D finished

--- app/controllers/logins_controllers.rb
+++ app/controllers/logins_controllers.rb
@@ -4,5 +4,20 @@ end
 
 MyApp.post "/logins/create" do
   @user = User.find_by_username(params[:username])
-  redirect "/users/<%=@user.id%>/profile"
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}/profile"
+  else
+    redirect "/logins/new"
+  end
+end
+
+MyApp.get "/logins/delete" do
+  @current_user = User.find_by_id(session[:user_id])
+  if @current_user == nil
+    redirect "/"
+  else
+    session[:user_id] == nil
+    redirect "/"
+  end
 end
