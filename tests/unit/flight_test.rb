750d62e1df8d0edbbd3d0fe4b9d1894b4604711f
Thorin Butler <thorin.butler@gmail.com>
2016-03-02 15:34:48 -0600

    broken flight test added

---
+++ tests/unit/flight_test.rb
@@ -0,0 +1,140 @@
+require 'test_helper'
+
+class FlightTest < Minitest::Test
+
+  def setup
+    @response_data = {"kind"=>"qpxExpress#tripsSearch",
+ "trips"=>
+  {"kind"=>"qpxexpress#tripOptions",
+   "requestId"=>"egEUZvC2pbOw3L7dZ0NtT3",
+   "data"=>
+    {"kind"=>"qpxexpress#data",
+     "airport"=>
+      [{"kind"=>"qpxexpress#airportData", "code"=>"DEN", "city"=>"DEN", "name"=>"Denver International"},
+       {"kind"=>"qpxexpress#airportData", "code"=>"LAX", "city"=>"LAX", "name"=>"Los Angeles International"},
+       {"kind"=>"qpxexpress#airportData", "code"=>"OMA", "city"=>"OMA", "name"=>"Omaha Eppley Airfield"}],
+     "city"=>
+      [{"kind"=>"qpxexpress#cityData", "code"=>"DEN", "name"=>"Denver"},
+       {"kind"=>"qpxexpress#cityData", "code"=>"LAX", "name"=>"Los Angeles"},
+       {"kind"=>"qpxexpress#cityData", "code"=>"OMA", "name"=>"Omaha"}],
+     "aircraft"=>
+      [{"kind"=>"qpxexpress#aircraftData", "code"=>"753", "name"=>"Boeing 757"},
+       {"kind"=>"qpxexpress#aircraftData", "code"=>"CR7", "name"=>"Canadair RJ 700"}],
+     "tax"=>
+      [{"kind"=>"qpxexpress#taxData", "id"=>"ZP", "name"=>"US Flight Segment Tax"},
+       {"kind"=>"qpxexpress#taxData", "id"=>"AY_001", "name"=>"US September 11th Security Fee"},
+       {"kind"=>"qpxexpress#taxData", "id"=>"US_001", "name"=>"US Transportation Tax"},
+       {"kind"=>"qpxexpress#taxData", "id"=>"XF", "name"=>"US Passenger Facility Charge"}],
+     "carrier"=>[{"kind"=>"qpxexpress#carrierData", "code"=>"UA", "name"=>"United Airlines, Inc."}]},
+   "tripOption"=>
+    [{"kind"=>"qpxexpress#tripOption",
+      "saleTotal"=>"USD185.10",
+      "id"=>"dSbBnVALjtVLnt3T8ApEB1001",
+      "slice"=>
+       [{"kind"=>"qpxexpress#sliceInfo",
+         "duration"=>292,
+         "segment"=>
+          [{"kind"=>"qpxexpress#segmentInfo",
+            "duration"=>102,
+            "flight"=>{"carrier"=>"UA", "number"=>"4866"},
+            "id"=>"GmfET45UMB6ku437",
+            "cabin"=>"COACH",
+            "bookingCode"=>"L",
+            "bookingCodeCount"=>9,
+            "marriedSegmentGroup"=>"0",
+            "leg"=>
+             [{"kind"=>"qpxexpress#legInfo",
+               "id"=>"LpIQYD-vop0uMlXu",
+               "aircraft"=>"CR7",
+               "arrivalTime"=>"2016-04-12T18:15-06:00",
+               "departureTime"=>"2016-04-12T17:33-05:00",
+               "origin"=>"OMA",
+               "destination"=>"DEN",
+               "duration"=>102,
+               "operatingDisclosure"=>"OPERATED BY GOJET AIRLINES DBA UNITED EXPRESS",
+               "mileage"=>470,
+               "secure"=>true}],
+            "connectionDuration"=>42},
+           {"kind"=>"qpxexpress#segmentInfo",
+            "duration"=>148,
+            "flight"=>{"carrier"=>"UA", "number"=>"701"},
+            "id"=>"G+hLwis5erHh-8nY",
+            "cabin"=>"COACH",
+            "bookingCode"=>"L",
+            "bookingCodeCount"=>9,
+            "marriedSegmentGroup"=>"0",
+            "leg"=>
+             [{"kind"=>"qpxexpress#legInfo",
+               "id"=>"LHFhjrxS2mizDXG+",
+               "aircraft"=>"753",
+               "arrivalTime"=>"2016-04-12T20:25-07:00",
+               "departureTime"=>"2016-04-12T18:57-06:00",
+               "origin"=>"DEN",
+               "destination"=>"LAX",
+               "destinationTerminal"=>"7",
+               "duration"=>148,
+               "onTimePerformance"=>80,
+               "mileage"=>860,
+               "meal"=>"Food and Beverages for Purchase",
+               "secure"=>true}]}]}],
+      "pricing"=>
+       [{"kind"=>"qpxexpress#pricingInfo",
+         "fare"=>
+          [{"kind"=>"qpxexpress#fareInfo",
+            "id"=>"An16pZQ/LWBBdynZ+ANY8p15UT4khXHaoJbaPd0uUVSo",
+            "carrier"=>"UA",
+            "origin"=>"OMA",
+            "destination"=>"LAX",
+            "basisCode"=>"LAK14AKS"}],
+         "segmentPricing"=>
+          [{"kind"=>"qpxexpress#segmentPricing",
+            "fareId"=>"An16pZQ/LWBBdynZ+ANY8p15UT4khXHaoJbaPd0uUVSo",
+            "segmentId"=>"GmfET45UMB6ku437"},
+           {"kind"=>"qpxexpress#segmentPricing",
+            "fareId"=>"An16pZQ/LWBBdynZ+ANY8p15UT4khXHaoJbaPd0uUVSo",
+            "segmentId"=>"G+hLwis5erHh-8nY"}],
+         "baseFareTotal"=>"USD155.35",
+         "saleFareTotal"=>"USD155.35",
+         "saleTaxTotal"=>"USD29.75",
+         "saleTotal"=>"USD185.10",
+         "passengers"=>{"kind"=>"qpxexpress#passengerCounts", "adultCount"=>1},
+         "tax"=>
+          [{"kind"=>"qpxexpress#taxInfo",
+            "id"=>"US_001",
+            "chargeType"=>"GOVERNMENT",
+            "code"=>"US",
+            "country"=>"US",
+            "salePrice"=>"USD11.65"},
+           {"kind"=>"qpxexpress#taxInfo",
+            "id"=>"AY_001",
+            "chargeType"=>"GOVERNMENT",
+            "code"=>"AY",
+            "country"=>"US",
+            "salePrice"=>"USD5.60"},
+           {"kind"=>"qpxexpress#taxInfo",
+            "id"=>"ZP",
+            "chargeType"=>"GOVERNMENT",
+            "code"=>"ZP",
+            "country"=>"US",
+            "salePrice"=>"USD8.00"},
+           {"kind"=>"qpxexpress#taxInfo",
+            "id"=>"XF",
+            "chargeType"=>"GOVERNMENT",
+            "code"=>"XF",
+            "country"=>"US",
+            "salePrice"=>"USD4.50"}],
+         "fareCalculation"=>
+          "OMA UA X/DEN UA LAX 155.35LAK14AKS USD 155.35 END ZP OMA DEN XT 11.65US 8.00ZP 5.60AY 4.50XF DEN4.50",
+         "latestTicketingTime"=>"2016-03-03T23:59-05:00",
+         "ptc"=>"ADT"}]}]}}
+
+  end
+
+  def test_get_total_cost
+    # @response_data....
+
+    x = "Foo"
+
+    assert_includes("155.35", x)
+  end
+end
