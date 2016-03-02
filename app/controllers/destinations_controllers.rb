fcbf157f52e485192a75d39230479780bf371a79
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-02 11:23:21 -0600

    Destination CRUD working

--- app/controllers/destinations_controllers.rb
+++ app/controllers/destinations_controllers.rb
@@ -14,3 +14,22 @@ MyApp.get "/destinations/index" do
   @destinations = Destination.all
   erb :"/destinations/index"
 end
+
+MyApp.get "/destinations/:id/edit" do
+  @destination = Destination.find_by_id(params[:id])
+  erb :"/destinations/edit"
+end
+
+MyApp.post "/destinations/:id/update" do
+  @destination = Destination.find_by_id(params[:id])
+  @destination.name = params[:name]
+  @destination.airport_code = params[:airport_code]
+  @destination.save
+  redirect "/destinations/index"
+end
+
+MyApp.post "/destinations/:id/delete" do
+  @destination = Destination.find_by_id(params[:id])
+  @destination.delete
+  redirect "/destinations/index"
+end
