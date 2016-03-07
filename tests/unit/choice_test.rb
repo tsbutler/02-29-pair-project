fc6bd387b97af2d7a84d2d514e2b4804f0879cc3
Thorin Butler <thorin.butler@gmail.com>
2016-03-07 14:50:09 -0600

    uncommunicative variables made more communicative in the choice tests

--- tests/unit/choice_test.rb
+++ tests/unit/choice_test.rb
@@ -3,36 +3,36 @@ require 'test_helper'
 class ChoiceTest < Minitest::Test
 
   def setup
-    @user_1 = User.new
-    @user_1.save
+    @user_object_for_tests_1 = User.new
+    @user_object_for_tests_1.save
 
-    @user_2 = User.new
-    @user_2.save
+    @user_object_for_tests_2 = User.new
+    @user_object_for_tests_2.save
 
-    @choice_1 = Choice.new
-    @choice_1.destination_id = 1
-    @choice_1.user_id = @user_1.id
-    @choice_1.save
+    @user_choice_1 = Choice.new
+    @user_choice_1.destination_id = 1
+    @user_choice_1.user_id = @user_1.id
+    @user_choice_1.save
 
-    @choice_2 = Choice.new
-    @choice_2.destination_id = nil
-    @choice_2.user_id = @user_2.id
-    @choice_2.save
+    @user_choice_2 = Choice.new
+    @user_choice_2.destination_id = nil
+    @user_choice_2.user_id = @user_2.id
+    @user_choice_2.save
   end
 
   def test_set_errors
-    assert_includes(@choice_2.set_errors, "Destination cannot be blank.")
-    assert_equal(@choice_1.set_errors, nil)
+    assert_includes(@user_choice_2.set_errors, "Destination cannot be blank.")
+    assert_equal(@user_choice_1.set_errors, nil)
   end
 
   def test_is_valid?
-    assert_equal(@choice_1.is_valid?, true)
-    assert_equal(@choice_2.is_valid?, false)
+    assert_equal(@user_choice_1.is_valid?, true)
+    assert_equal(@user_choice_2.is_valid?, false)
   end
 
   def test_get_errors
-    assert_includes(@choice_2.get_errors, "Destination cannot be blank.")
-    assert_equal(@choice_1.get_errors, [])
+    assert_includes(@user_choice_2.get_errors, "Destination cannot be blank.")
+    assert_equal(@user_choice_1.get_errors, [])
   end
 
 end
